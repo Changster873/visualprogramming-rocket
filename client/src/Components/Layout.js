ab92a679c7bf9966273ae11c1f5f5517b60442de
Chansocheat Chheang <chheanchan@myvuw.ac.nz>
2020-08-10 13:16:26 +1200

    implemented really basic functionality and got it to move

--- client/src/Components/Layout.js
+++ client/src/Components/Layout.js
@@ -2,6 +2,23 @@ import React, {useRef} from 'react';
 import Blockly from 'blockly';
 import Canvas from './Canvas';
 
+// fields to pass to the canvas to draw
+let fields = {
+    xChange: 0,
+    yChange: 0,
+    speed: 5,
+    ready: false,
+    commands: []
+}
+
+// changing relevant fields and get canvas to draw it
+const changeFields = () => {
+    fields.ready = true
+    fields.xChange += 100
+    fields.yChange += 0
+    fields.speed = 1;
+}
+
 class Layout extends React.Component {
 
     workspace; // the visual block toolbox
@@ -11,29 +28,34 @@ class Layout extends React.Component {
      * Before components load, make them first.
      */
     componentWillMount() {
-        this.defineLaunch();
+        this.defineBlocks();
         this.defined = true;
     }
 
     /**
-     * Define the Launch block.
+     * Define the custom blocks.
      */
-    defineLaunch() {
+    defineBlocks() {
 
         Blockly.Blocks['launch'] = {
             init: function() {
-            this.appendDummyInput().appendField('Launch');
-              this.setNextStatement(true);
-              this.setPreviousStatement(false);
-              this.setColour(160);
-              this.setOutput(true, String)
+                this.appendDummyInput().appendField('Launch');
+                this.setNextStatement(true);
+                this.setPreviousStatement(false);
+                this.setColour(160);
             }
           };
-
-        Blockly.JavaScript['launch'] = function(block) {
-            var arg0 = Blockly.JavaScript.valueToCode(block, 'VALUE', Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
-            return ['var rocket = new Rocket(); \n rocket.launch()', Blockly.JavaScript.ORDER_MEMBER];
-        }
+        
+        Blockly.Blocks['speed'] = {
+            init: function() {
+                this.appendValueInput('VALUE')
+                    .setCheck('Number')
+                    .appendField('Speed')
+                this.setNextStatement(true);
+                this.setPreviousStatement(true);
+                this.setColour(160);
+            }
+        };
     }
 
     /**
@@ -41,49 +63,62 @@ class Layout extends React.Component {
      * @param {*} change 
      */
     javascriptGeneration(change) {
-        var generation = Blockly.JavaScript.workspaceToCode(this.workspace);
-        document.getElementById('code').value = generation;
+
+        // all the instructions in the blockly editor
+        var list = this.workspace.topBlocks_;
+        // re-initialise commands list so no duplicates of the same things when event is triggered
+        fields.commands = []
+        // parse to get their types
+        for (let i=0; i<list.length; i++) {
+            fields.commands.push(this.workspace.topBlocks_[i].type)
+        }
     }
 
     render() {
-        console.log(this.defined);
         return (
             <div className="wrapper">
                 {/* blocks for visual programming */}
                 <xml id="toolbox" style={{display: `none`}}>
-                    <block type="controls_if"></block>
+                    {/* <block type="controls_if"></block>
                     <block type="controls_repeat_ext"></block>
                     <block type="logic_compare"></block>
-                    <block type="math_number"></block>
+                    
                     <block type="math_arithmetic"></block>
                     <block type="text"></block>
-                    <block type="text_print"></block>
+                    <block type="text_print"></block> */}
+                    <block type="math_number"></block>
                     { this.defined && <block type="launch"></block>}
+                    { this.defined && <block type="speed"></block>}
                 </xml>
-                {/* visual editor */}
-                <div className="editor">
-                    <div className="blocklyDiv" 
-                        style={{width: 800, height: 1000}} 
-                        ref={(ref) => {
-                            this.workspace = Blockly.inject(ref,
-                            {toolbox: document.getElementById('toolbox')});
-                        }}
-                        // content changes after dragging and dropping
-                        onMouseMove={() => this.javascriptGeneration()} 
-                        onClick={() => this.javascriptGeneration()}
-                        onBlur={() => this.javascriptGeneration()}
-                        >
+                <div className="elements">
+                    {/* visual editor */}
+                    <div className="editor">
+                        <div className="blocklyDiv" 
+                            style={{width: 800, height: 1000}} 
+                            ref={(ref) => {
+                                this.workspace = Blockly.inject(ref,
+                                {toolbox: document.getElementById('toolbox')});
+                            }}
+                            // content changes after dragging and dropping
+                            onMouseMove={() => this.javascriptGeneration()} 
+                            onClick={() => this.javascriptGeneration()}
+                            onBlur={() => this.javascriptGeneration()}
+                            >
+                        </div>
+                        {/* code generated from editor */}
+                        <div style={{margin: 10}}>
+                            <h1 style={{color: `grey`}}> Code Generated From Above </h1>
+                            <input id="code" style={{width: 800, height: 200, fontSize: 36}} placeholder="None"/>
+                        </div>
                     </div>
-                    {/* code generated from editor */}
-                    <div style={{margin: 10}}>
-                        <h1 style={{color: `grey`}}> Code Generated From Above </h1>
-                        <input id="code" style={{width: 800, height: 200, fontSize: 36}} placeholder="None"/>
+                    {/* rocket game simulation */}
+                    <div className="simulation">
+                    <button style={{width: 100, zIndex: 10}} onClick={() => {changeFields();}}>Run/Start</button>
+                    <button style={{width: 100, zIndex: 10}} onClick={() => {fields.ready = false}}>Pause</button>
+                        <Canvas fields={fields}/>
                     </div>
                 </div>
-                {/* rocket game simulation */}
-                <div className="simulation">
-                    <Canvas />
-                </div>
+                
             </div>
         );
     }
